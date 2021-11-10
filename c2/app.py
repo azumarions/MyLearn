f686b57b2c994a58460f9da8182b287c9dc05dbf
azuma <azuma20011224@gmail.com>
2021-11-11 00:46:13 +0900

    this is flask sample app.

---
+++ c2/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html',
+    title = "form sample",
+    message = "what's your name?")
+
+@app.route('/next')
+def next():
+    return render_template('next.html')
+
+@app.route('/', methods=['POST'])
+def form():
+    ck = request.form.get('check')
+    rd = request.form.get('radio')
+    sel = request.form.getlist('sel')
+    return render_template('index.html',
+    title = "form sample",
+    message = [ck, rd, sel])
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost')
