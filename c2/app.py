d43284679e6123df6d7de7e9156ac825806679c9
azuma <azuma20011224@gmail.com>
2021-11-11 02:27:32 +0900

    learning jinja statements

--- c2/app.py
+++ c2/app.py
@@ -4,22 +4,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    data = ['windows', 'macOS', 'linuxOS', 'chromeOS']
     return render_template('index.html',
-    title = "form sample",
-    message = "what's your name?")
+    title = 'this is jinja template!',
+    message = data,
+    data = data)
 
 @app.route('/next')
 def next():
     return render_template('next.html')
 
-@app.route('/', methods=['POST'])
-def form():
-    ck = request.form.get('check')
-    rd = request.form.get('radio')
-    sel = request.form.getlist('sel')
-    return render_template('index.html',
-    title = "form sample",
-    message = [ck, rd, sel])
+# @app.route('/', methods=['POST'])
+# def form():
+#     ck = request.form.get('check')
+#     rd = request.form.get('radio')
+#     sel = request.form.getlist('sel')
+#     return render_template('index.html',
+#     title = "form sample",
+#     message = [ck, rd, sel])
 
 if __name__ == '__main__':
     app.debug = True
