d32f0d0d432d907935404400ba0f0f77121bf436
azuma <azuma20011224@gmail.com>
2021-11-11 15:56:36 +0900

    flask jinja sample, create calculate sum filter

--- c2/app.py
+++ c2/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    n = random.randrange(5, 10)
+    data = []
+    for n in range(n):
+        data.append(random.randrange(0, 100))
     return render_template('index.html',
     title = 'Template sample',
-    message = 'this is sample page.')
+    message = 'calculate sum',
+    data = data)
 
 @app.route('/next')
 def next():
@@ -14,6 +20,15 @@ def next():
     title = 'next page',
     message = 'this is another page sample.',
     data = ['one', 'two', 'three'])
+
+@app.template_filter('sum')
+def sum_filter(data):
+    total = 0
+    for item in data:
+        total += item
+    return total
+
+app.jinja_env.filters['sum'] = sum_filter
 
 # @app.route('/', methods=['POST'])
 # def form():
