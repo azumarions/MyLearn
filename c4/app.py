cab7116912158605835bbe49918d4d793462b7d0
azuma <azuma20011224@gmail.com>
2021-11-20 02:29:36 +0900

    create QRcode message app,and edit app.py

--- c4/app.py
+++ c4/app.py
@@ -1,91 +1,149 @@
-from flask import Flask, render_template, request, session, redirect, jsonify
+from logging import PercentStyle
+from os import O_RANDOM
+from re import T
+from flask import Flask, render_template, request, session, redirect, jsonify, current_app, g
+
+import sqlite3
+import json
+from datetime import datetime
+
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm.exc import NoResultFound
+
+import qrcode as qr
+from PIL import Image
+import base64
+from io import BytesIO
 import pickle
+
+from sqlalchemy.orm.session import Session
 
 
 app = Flask(__name__)
 app.secret_key = b'random string...'
 
+engine = create_engine('sqlite:///sample.sqlite3')
 
-member_data = {}
-message_data = []
-member_data_file = 'member_data.dat'
-message_data_file = 'message_data.dat'
+# get QRCode data.
+def get_qrdata(s):
+    qr_img = qr.make(s)
 
+    byte_buf = BytesIO()
+    qr_img.save(byte_buf, format="png")
+    qr_data = byte_buf.getvalue()
+    b64_data = 'data:image/png;base64,' + base64.b64encode(qr_data).decode("utf-8")
+    return b64_data
 
-# load member_data from file.
-try:
-    with open(member_data_file, "rb") as f:
-        list = pickle.load(f)
-        if list != None:
-            member_data = list
-except:
-    pass
+# base model
+Base = declarative_base()
 
+# model class
+class User(Base):
+    __tablename__ = 'users'
 
-# load message_data from file.
-try:
-    with open(message_data_file, "rb") as f:
-        list = pickle.load(f)
-        if list != None:
-            message_data = list
-except:
-    pass
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    password = Column(String(255))
 
+    def to_dict(self):
+        return {
+            'id': int(self.id),
+            'name': str(self.name),
+            'password': str(self.password)
+        }
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    message = Column(String(255))
+    created = Column(DateTime())
+
+    user = relationship('User')
+    
+    def to_dict(self):
+        return {
+            'id': int(self.id),
+            'user_id': int(self.user_id),
+            'message': str(self.message),
+            'created': str(self.created),
+            'user': str(self.user.name)
+        }
+
+# get Model-list Dictionary
+def get_by_list(arr):
+    res = []
+    for item in arr:
+        res.append(item.to_dict())
+    return res
 
 # access top page.
 @app.route('/', methods=['GET'])
 def index():
-    global message_data
-    return render_template('message.html', \
-        login=False, \
-        title='Messages', \
-        message='not logined...', 
-        data=message_data )
+    return render_template('index.html',
+    login = False,
+    title = 'Messages',
+    message = 'not logined....',
+    data = [])
 
-
-# post message.
+# post message
 @app.route('/post', methods=['POST'])
-def postMsg():
-    global message_data
+def post_msg():
     id = request.form.get('id')
-    msg = request.form.get('comment')
-    message_data.append((id, msg))
-    if len(message_data) > 25:
-        message_data.pop(0)
-    try:
-        with open(message_data_file, 'wb') as f:
-            pickle.dump(message_data, f)
-    except:
-        pass
+    msg = request.form.get('message')
+    created = datetime.now()
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    msg_obj = Message(user_id=id, message=msg, created=created)
+    ses.add(msg_obj)
+    ses.commit()
+    ses.close()
     return 'True'
-
 
 # get messages.
 @app.route('/messages', methods=['POST'])
-def getMsg():
-    global message_data
-    return jsonify(message_data)
+def get_msg():
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    re = ses.query(Message).join(User).order_by(Message.created.desc())[:10]
+    msgs = get_by_list(re)
+    return jsonify(msgs)
 
+# get QRcode Data
+@app.route('/qr', methods=['POST'])
+def get_qr():
+    id = request.form.get('id')
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    re = ses.query(Message).filter(Message.id == id).one()
+    dic = re.to_dict()
+    dic['qr'] = get_qrdata(re.message)
+    return jsonify(dic)
 
 # login form sended.
 @app.route('/login', methods=['POST'])
 def login_post():
-    global member_data, message_data
-    id = request.form.get('id')
-    pswd = request.form.get('pass')
-    if id in member_data:
-        if pswd == member_data[id]:
-            flg = 'True'
+    name = request.form.get('name')
+    pswd = request.form.get('password')
+
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    n = ses.query(User).filter(User.name == name).count()
+    if n == 0:
+        usr = User(name=name, password=pswd)
+        ses.add(usr)
+        ses.commit()
+        flg = str(usr.id)
+    else:
+        usr = ses.query(User).filter(User.name == name).one()
+        if pswd == usr.password:
+            flg = str(usr.id)
         else:
             flg = 'False'
-    else:
-        member_data[id] = pswd
-        flg = 'True'
-        try:
-            with open(member_data_file, 'wb') as f:
-                pickle.dump(member_data, f)
-        except:
-            pass
+    ses.close()
     return flg
 
 if __name__ == '__main__':
